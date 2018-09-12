c83d25db2f29614ab4df4aabccb946220eba0276
Teera Binkasem <bteera@gmail.com>
2018-09-12 17:11:17 +0700

    test reg

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDunESnu_j2S5uzipeaKYgsemj0YrO-RrM",
+    authDomain: "authprofile-41b15.firebaseapp.com",
+    databaseURL: "https://authprofile-41b15.firebaseio.com",
+    projectId: "authprofile-41b15",
+    storageBucket: "authprofile-41b15.appspot.com",
+    messagingSenderId: "7252902368"
+}
